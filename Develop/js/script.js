990c1df5295fb15cd1c13e2b7fa8b553791895b7
DeAnna Boiani <deannaboiani@gmail.com>
2021-09-26 15:28:45 -0700

    question 1 on start button and timer working

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -13,93 +13,113 @@
 // element.preventDefualt()
 
 var srtBtn = document.getElementById("start-button");
-var question = document.getElementById("question-type");
-var answers = document.getElementById("answer-option")
+var qContainer = document.getElementById("quesiton-container");
+var questionEl = document.getElementById("question-type");
+var answerEl = document.getElementById("answer-button")
 var firstAnswer = document.getElementById("answer-1");
 var secondAnswer = document.getElementById("answer-2");
 var thirdAnswer = document.getElementById("answer-3");
 var fourthAnswer = document.getElementById("answer-4");
-var select = 0;
 var timer = document.getElementById("timer-count");
 var initials = document.getElementById("initals")
 var score = document.getElementById("intitals")
-var submit =document.getElementById("submit");
+var submit = document.getElementById("submit");
 var secondsLeft = 60;
-var questions = [
+var questionArray = [
     {
-    question : "Question 1?",
-
-    firstAnswer : "Correct",
-
-    secondAnswer : "Wrong",
-
-    thirdAnswer : "Wrong",
-
-    correct : "firstAnswer"
-    },{
-    question : "Question 2?",
-
-    firstAnswer : "Wrong",
-
-    secondAnswer : "Correct",
-
-    thirdAnswer : "Wrong",
-
-    correct : "secondAnswer"
-    },{
-    question : "Question 3?",
-
-    firstAnswer : "Correct",
-
-    secondAnswer : "Wrong",
-
-    thirdAnswer : "Wrong",
-
-    correct : "firstAnswer"
+        question: "Question 1?",
+        answers: [
+            {text: 'answer 1', correct: true},
+            {text: 'answer 2', correct: false},
+            {text: 'answer 3', correct: false},
+            {text: 'answer 4', correct: false},
+        ]
+    }, {
+        question: "Question 2?",
+        answers: [
+            {text: 'answer 1', correct: true},
+            {text: 'answer 2', correct: false},
+            {text: 'answer 3', correct: false},
+            {text: 'answer 4', correct: false},
+        ]
+    }, {
+        question: "Question 3?",
+        answers: [
+            {text: 'answer 1', correct: true},
+            {text: 'answer 2', correct: false},
+            {text: 'answer 3', correct: false},
+            {text: 'answer 4', correct: false},
+        ]
+    },{ question: "Question 4?",
+        answers: [
+            {text: 'answer 1', correct: true},
+            {text: 'answer 2', correct: false},
+            {text: 'answer 3', correct: false},
+            {text: 'answer 4', correct: false},
+        ]
     }
 ];
 
+// on click of start button, the quiz will start
+srtBtn.addEventListener("click", startQuiz) 
 
+function startQuiz() {
+    console.log('started')
+    // start button is hidden when quiz starts
+    srtBtn.style.visibility = "hidden";
 
-function playGame() {
+    // qContainer.style.visibility = "visible;"
+    nextQuestion = questionArray.sort();
+    currentQuestion = 0
+    console.log(nextQuestion);
+    getQuestion();
+    setTime();
 
-    // on click of start button, start timer and show first question
-    srtBtn.addEventListener("click", function() {
-        showQuestion();
-
-        setTime();
-        });
-
-
-// if quesiton is not shown, show question
-  function showQuestion(){
-    if (question.style.display === 'none') {
-        question.setAttribute("class", "display"); 
-        answer.setAttribute("class", "display")
-        getQuestion();
-    }
 };
 
-// add questions and answers from array
-function getQuestion(){
-    question.textContent = questions[select].question;
-    firstAnswer.textContent = answers[select].firstAnswer;
-    secondAnswer.textContent = answers[select].secondAnswer;
-    thirdAnswer.textContent = answers[select].thirdAnswer;
-    correct = questions[select].correct;
+// from question array, the next question is shown
+function nextQuestion() {
+    getQuestion(nextQuestion[currentQuestion]);
+         
+};
+
+
+    // add questions and answers from array
+function getQuestion() {
+    questionEl.textContent = questionArray[currentQuestion].question;
+    firstAnswer.textContent = questionArray[currentQuestion].answers;
+    secondAnswer.textContent = questionArray[currentQuestion].answers;
+    thirdAnswer.textContent = questionArray[currentQuestion].answers;
+    fourthAnswer.textContent = questionArray[currentQuestion].answers;
+    console.log(firstAnswer, secondAnswer, thirdAnswer, fourthAnswer);
 }
-    
-// timer counting down seconds from 60
+
+
+
+   
+
+    // function checkAnswer(answer) {
+    //     if (questionArray[currentQuestion].correct == answer) {
+    //         correctAnswer();
+    // }   else {
+    //         wrongAnswer();
+
+    //     }
+    // }  
+
+
+// // timer counting down seconds from 60
   function setTime() {
 
-    var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = secondsLeft + " seconds left until game over";
- 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-    }
-   
-  }, 1000);
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timer.textContent = secondsLeft + " seconds left until game over";
 
-  }}
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+            }
+
+        }, 1000);
+
+    }
+// }
